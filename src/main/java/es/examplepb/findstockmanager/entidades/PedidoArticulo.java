c73d7f3799118699fe430378f5c3762c1787e517
pbalhico <pbalhico@gmail.com>
2025-04-18 18:59:59 +0200

    modificaciones entidades

--- src/main/java/es/examplepb/findstockmanager/entidades/PedidoArticulo.java
+++ src/main/java/es/examplepb/findstockmanager/entidades/PedidoArticulo.java
@@ -1,19 +1,4 @@
 package es.examplepb.findstockmanager.entidades;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-@Entity
-@Table(name = "pedido_articulo")
-@Data
-@NoArgsConstructor
 public class PedidoArticulo {
-
-    @EmbeddedId
-    private PedidoArticuloId id;
-
-    @Column(name = "cantidad_pedido_articulo")
-    private Integer cantidadPedidoArticulo;
-    private Double importeTotal;
-
 }
